99d907fa0c2b18f6ee2bcec800a2602c70121e76
Kyle Andre David Lim <119409982+kylemastercoder14@users.noreply.github.com>
2023-06-24 17:36:47 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,15 @@ import useInfoModalStore from '@/hooks/useInfoModalStore';
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
   return {
     props: {}
   }
